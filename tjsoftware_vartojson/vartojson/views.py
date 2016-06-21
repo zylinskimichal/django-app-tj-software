39996805863c19ef357040309053c05a13ddf7a3
Michał Żyliński <m.zylinski@o2.pl>
2016-06-21 13:51:04 +0200

    4 supporting number translation

--- tjsoftware_vartojson/vartojson/views.py
+++ tjsoftware_vartojson/vartojson/views.py
@@ -1,5 +1,12 @@
 from django.http import JsonResponse
+from num2words import num2words
 
 
 def index(request):
-    return JsonResponse({'liczba': request.POST.get('bar', 0)})
+    return JsonResponse({
+        request.POST.get('liczba', 0):
+        num2words(
+                request.POST.get('liczba', 0),
+                lang='en',
+        )
+    })
